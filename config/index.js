12b2b020de5fc587a6c39d550964ecaf855fc727
stillet887 <stillet887@gmail.com>
2018-02-05 16:05:26 +0300

    Сonfigгate project for placement on Github Pages through Travis CI

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'https://stillet887.github.io/learning-vue/',
 
     /**
      * Source Maps
